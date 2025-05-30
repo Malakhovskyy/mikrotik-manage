d1e335f423873b5eb7fb542b1d1bf1f2cca7ae4c
Malakhovskyy <malakhovskyy@outlook.com>
2025-05-30 13:19:36 +0300

    fix

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 engine = create_async_engine(settings.database_url, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
